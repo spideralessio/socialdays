eeb466b3a5c6f746c6c9b268f4f7ed0921f0a88a
Alessio Ragno <spideralessio97@gmail.com>
2017-07-04 21:57:33 +0200

    Aggiornamento Associazioni

--- assets/images/associazioni/associazioni.py
+++ assets/images/associazioni/associazioni.py
@@ -9,7 +9,7 @@ for e in l:
 	if not ".txt" in e and not ".py" in e and not ".pyc" in e:
 		n += 1
 		css += ".partner .partner-box-%d {\n  background: url(../images/associazioni/%s) no-repeat center center/contain;\n}\n" % (n,e)
-		html += "<div class=\"col-sm-2\">\n\t<a class=\"partner-box partner-box-%d\"></a>\n</div>" % (n)
+		html += "<div class=\"col-sm-2\">\n\t<a class=\"partner-box partner-box-%d\"></a>\n</div>\n" % (n)
 
 print(css)
 

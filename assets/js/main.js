81591a7cb9eedee536256c459a4df1a0ec4272e9
Alessio Ragno <spideralessio97@gmail.com>
2017-07-04 22:21:54 +0200

    Modifica Icone e Fix Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,45 +25,14 @@ function handleTopNavAnimation() {
  * Registration Form
 */
 
-$('#registration-form').submit(function(e){
-    e.preventDefault();
-    
-    var postForm = { //Fetch form data
-            'fname'     : $('#registration-form #fname').val(),
-            'lname'     : $('#registration-form #lname').val(),
-            'email'     : $('#registration-form #email').val(),
-            'cell'      : $('#registration-form #cell').val(),
-            'address'   : $('#registration-form #address').val(),
-            'zip'       : $('#registration-form #zip').val(),
-            'city'      : $('#registration-form #city').val(),
-            'program'   : $('#registration-form #program').val()
-    };
 
-    $.ajax({
-            type      : 'POST',
-            url       : './assets/php/contact.php',
-            data      : postForm,
-            dataType  : 'json',
-            success   : function(data) {
-                            if (data.success) {
-                                $('#registration-msg .alert').html("Registration Successful");
-                                $('#registration-msg .alert').removeClass("alert-danger");
-                                $('#registration-msg .alert').addClass("alert-success");
-                                $('#registration-msg').show();
-                            }
-                            else
-                            {
-                                $('#registration-msg .alert').html("Registration Failed");
-                                $('#registration-msg .alert').removeClass("alert-success");
-                                $('#registration-msg .alert').addClass("alert-danger");
-                                $('#registration-msg').show();
-                            }
-                        }
-        });
-});
 
 /*
  * SmoothScroll
 */
 
 smoothScroll.init();
+
+$("a[data-scroll]").click(function(){
+    $("#menu-button").click()
+});
